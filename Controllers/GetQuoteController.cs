318ffded8afe46f09eb1cd5d7e9524b5a58621bb
benjaminHolsinger <bholsinger@gmail.com>
2022-07-10 15:22:15 -0600

    added connection string (needs work)

--- Controllers/GetQuoteController.cs
+++ Controllers/GetQuoteController.cs
@@ -1,4 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcMovie.Models;
+using System;
+
+
+
+
+using (SqlConnection connection = new SqlConnection(  
+  "Integrated Security=SSPI;Initial Catalog=Northwind"))  
+    {  
+        connection.Open();
+        var Server=myServerAddress;
+        var Database=myDataBase;
+        var Uid=myUsername;
+        var Pwd=myPassword;
+        
+    } 
+
+
 
 
